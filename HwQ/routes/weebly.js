3e5317f257fa2284dffb90dba93f5659f70cb251
Ndju <Ndju@192.168.169.6>
2015-12-29 21:02:01 -0800

    added comments to weebly.js

--- HwQ/routes/weebly.js
+++ HwQ/routes/weebly.js
@@ -9,13 +9,16 @@ exports.getData = function( callback ){
 		  if (!error && response.statusCode === 200) {
 		    //console.log(body) // Show the HTML for the Google homepage. 
    		    var rows = [];
-		    
+		    //reads the csv file, each comma seperated section is a new value in an array (header: false means that not read as dictionary)
 		    csv.fromString(body, {headers: false})
+		    //only reads when there is data
 		    .on("data", function(data){
 				if (data[0] !== "Date Submitted") {
+					//the first line on weebly's submission csv file is the column headers, only pushes if not header
 					rows.push( data );
 				}
 		    })
+		    //no more data, print on console "done!!!!"
 		    .on("end", function(){
 		        callback ( rows );
 		        console.log("done!!!!");
