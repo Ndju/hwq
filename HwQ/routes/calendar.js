cc2755f43d5a2d37f51e94118e8d976a4e344229
shivamjindal <jshivam17.5@gmail.com>
2015-12-30 23:32:22 -0800

    integrates calendar into program. no direct link now.

---
+++ HwQ/routes/calendar.js
@@ -0,0 +1,4 @@
+exports.calendar = function(req, res){
+  res.render('homepage', { /* Put the code that passes variables to homepage so that computer can get
+  user's files for the calendar here. check submissions for this) */ });
+};
